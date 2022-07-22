84395b438ac57b4069b8e370e2858cc2231a1a3b
Tivadar Cziko <72433224+cziko1@users.noreply.github.com>
2022-07-22 15:20:15 +0200

    Update MyFirstChatbot.py
    I stopped it this loop :+1:

--- ChatBot/MyFirstChatbot.py
+++ ChatBot/MyFirstChatbot.py
@@ -3,9 +3,18 @@ import time    # for waiting ...
 
 #print('Hello Mate!\nHow can I help you?')
 
+S = 1
+
 while True:
-    time.sleep(2) # it's a waiting time (in second)
-    pyautogui.typewrite('Hello Mate!\nHow Can I help you?') #'typewrite' it's a message
-    time.sleep(2)
-    pyautogui.press('enter') # When I pressed the 'enter' button after this code send a messange.
+    if S < 5:
+        S = S + 2
+        time.sleep(S) # it's a waiting time (in second)
+        pyautogui.typewrite('Hello Mate!')
+        time.sleep(S)
+        S = S + 3
+        pyautogui.typewrite('\nHow Can I help you?') #'typewrite' it's a message
+        pyautogui.press('enter') # When I pressed the 'enter' button after this code send a messange.
+
+    else:
+        break
 
